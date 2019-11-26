24a0c402fd2c029405a2899eaa97f5d225d113e1
shomnath <shomnath.pramanik@bjitgroup.com>
2019-11-26 09:33:31 +0600

    Initiate print function

---
+++ App.py
@@ -0,0 +1,12 @@
+print("Hello Triangle!!")
+
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+character_name = "David"
+character_age = 25.32465
+
+print("There once was a man named " + character_name + ",")
+print(character_age)
