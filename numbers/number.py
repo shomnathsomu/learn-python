9bd32db93ce25145f77610d2d0e3a033125f21ae
Shomnath Pramanik <shomnathcse22@gmail.com>
2019-11-26 04:59:25 +0000

    Finish number functions

---
+++ numbers/number.py
@@ -0,0 +1,11 @@
+from math import *
+my_number = 7
+print("Give me " + str(my_number) + " sci fi movies.")
+a = -10
+b = 3.6
+print(abs(a))
+print(pow(a, 3))
+print(round(b))
+print(floor(b))
+print(ceil(b))
+print(sqrt(36))
