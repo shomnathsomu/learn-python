3111061d99d1441faa14ab92820ea97e7867fc09
Shomnath Pramanik <shomnathcse22@gmail.com>
2019-11-26 04:04:04 +0000

    Finish string functions

---
+++ strings/string.py
@@ -0,0 +1,18 @@
+phrase = "\"Giraffe\nAcademy\""
+print(phrase)
+
+sentence = "A QUICK brown fox jumps over the lady dog"
+print(sentence.lower())
+print(sentence.upper())
+print(len(sentence))
+print(sentence.index("K"))
+print(sentence.index("brown"))
+
+A = "ABC"
+B = "def"
+print(A.isupper())
+print(A.islower())
+print(B.upper().isupper())
+print(B.replace("def", "DEF"))
+
+# string operation ends
