dfe4747b2de991718cfdcb7ceed2a031c1f5b4d5
Shomnath Pramanik <shomnathcse22@gmail.com>
2019-12-02 04:42:23 +0000

    Create forloop in py

---
+++ Loop/forloop.py
@@ -0,0 +1,20 @@
+for letter in "giraffe academy":
+    print(letter)
+
+print("")
+
+states = ["Arizona", "Boston", "California"]
+for name in states:
+    print(name)
+
+print("")
+
+# use range(a) from 0 to a
+for i in range(3, 10):
+    print(i)
+
+print("")
+
+for i in range(len(states)):
+    print(states[i])
+
