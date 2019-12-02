5f1c486dbe10c087f8c899f9bead431f7a63565a
Shomnath Pramanik <shomnathcse22@gmail.com>
2019-12-02 06:39:01 +0000

    Create translation in py

---
+++ strings/translation.py
@@ -0,0 +1,20 @@
+'''
+this also can be a comment
+'''
+
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation += "G"
+            else:
+                translation += "g"
+        else:
+            translation += letter
+
+    return translation
+
+
+print(translate(input("Enter a phrase: ")))
