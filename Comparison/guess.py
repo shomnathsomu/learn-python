4a8731aa71b0c6d44cce941c872b3f66bc7f1547
Shomnath Pramanik <shomnathcse22@gmail.com>
2019-12-02 03:47:07 +0000

    Update with props conditions

--- Comparison/guess.py
+++ Comparison/guess.py
@@ -1,7 +1,17 @@
 secret_word = "Ant"
 guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guess = False
 
-while guess != "Ant":
-    guess = input("Enter your guess: ")
+while guess != secret_word and not out_of_guess:
+    if guess_count < guess_limit:
+        guess = input("Enter your guess: ")
+        guess_count += 1
+    else:
+        out_of_guess = True
 
-print("You win!")
+if out_of_guess:
+    print("You're out of guesses. You lose!")
+else:
+    print("You win!")
