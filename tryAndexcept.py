7dcb63609c34aa0f5481a54404f3c86b710b8b2e
shomnath <shomnath.pramanik@bjitgroup.com>
2019-12-02 17:16:50 +0600

    Delete or move to another folder

--- tryAndexcept.py
+++
@@ -1,8 +0,0 @@
-try:
-    # num = 10/0
-    number = int(input("Enter a number: "))
-    print(number)
-except ZeroDivisionError as err:
-    print(err)
-except ValueError:
-    print("Invalid input")
