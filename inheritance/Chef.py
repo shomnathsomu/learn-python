07fea012a726d731ff26023a336ae425271a5a03
Shomnath Pramanik <shomnathcse22@gmail.com>
2019-12-03 08:27:59 +0000

    Create chef class

---
+++ inheritance/Chef.py
@@ -0,0 +1,12 @@
+class Chef:
+
+    @staticmethod
+    def make_chicken(self):
+        print("The chef makes a chicken.")
+
+    @staticmethod
+    def make_salad(self):
+        print("The chef makes a salad")
+
+    def make_special_dish(self):
+        print("The chef makes bbq ribs")
