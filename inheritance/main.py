6d61ad83f4558a9ae4836e2ce01ea801682c2fd9
Shomnath Pramanik <shomnathcse22@gmail.com>
2019-12-03 08:31:10 +0000

    Create inheritance objects

---
+++ inheritance/main.py
@@ -0,0 +1,10 @@
+from Chef import Chef
+from ChineseChef import ChineseChef
+
+myChef = Chef()
+myChef.make_special_dish()
+
+# object of inherited class
+myChineseChef = ChineseChef()
+myChineseChef.make_special_dish()
+myChineseChef.make_chicken()
