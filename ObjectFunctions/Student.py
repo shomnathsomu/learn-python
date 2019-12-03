682d01f3726d183e8dfa93e615bf193532834553
Shomnath Pramanik <shomnathcse22@gmail.com>
2019-12-03 06:41:50 +0000

    create object functions

---
+++ ObjectFunctions/Student.py
@@ -0,0 +1,8 @@
+from Student import Student
+
+student1 = Student("Oscar", "Accounting", 3.13)
+student2 = Student("Phyllis", "Business", 3.83)
+
+print(student2.on_honor_roll())
+print(student1.on_honor_roll())
+
